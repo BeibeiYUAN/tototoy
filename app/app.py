c3b373af08ffaf8daef797c86e0b93fc6f6629fd
BeibeiYuan <beibei_yuan@cargill.com>
2024-04-14 12:37:59 +0200

    env folder

--- app/app.py
+++ app/app.py
@@ -1,8 +1,11 @@
 from dash import Dash, html
+from flask import Flask
 
+# Exposing the Flask Server to enable configuring it for logging in
+server = Flask(__name__)
 app = Dash(__name__)
 
 app.layout = html.Div([html.Div(children="Hello Data")])
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run_server(debug=True)
